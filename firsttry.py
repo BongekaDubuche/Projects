33c223acd86981de987db30592b0db8fffd6640d
BongekaDubuche <bongeka.dubuche@gmail.com>
2021-11-02 12:37:04 -0600

    1st commit

--- firsttry.py
+++ firsttry.py
@@ -1,38 +1,38 @@
 import random
 
 
-username = input("Hi, What should we call you? ")
-print("Ok!," + username+" welcome to  Guessing Game!")
+username = input("Hi, What's your name? ")
+print("Ok!" + username + "Welcome to Guessing Game.!")
 
 def start_game():
 
 
+ number = random.randrange(1, 10)
 
-  number_to_guess = random.randrange(1, 10)
 
-
-  guess = input("Okay, I have picked a random number between 1 and 10. What's your guess? ")
-  try:
+guess = input("Okay, I have picked a random number between 1 and 10. What's your guess? ")
+try:
     int(guess)
-  except ValueError:
-        guess = input("That is not a valid number. Please input a number between 1-10. ")
-        guess = 1
+except 'ValueError':
+    guess = input("That is not a valid number. Please input a number between 1-10. ")
+    #guess = 1
 
-  while int(guess) != number_to_guess:
-        guess += 1
-  if int(guess) < number_to_guess:
+while guess != 'number_of_guess':
+    if guess < 'number':
         print("it's lower")
-  else:
+        guess = input("Guess again!")
+    if guess > 'number':
         print("it's higher")
-        guess = input("Your guess was {lower_or_higher} than the secret number. Guess again: ")
+        guess = input("Guess again! ")
+
         try:
             int(guess)
         except ValueError:
-         guess = input("{guess} isn't a valid entry. Please input a number between 1-10.")
-  else:
-        print("Good job!," + username + "! You got it right!")
-        print("Your guessed the number in ," + str(guess) + "tries!")
-        print("Game Over!!")
+            guess = input("{guess} isn't a valid entry. Please input a number between 1-10.")
+    else:
+         print("Good job!," + username + "! You got it right!")
+         print("Your guessed the number in ," + str(guess) + "tries!")
+         print("Game Over!!")
+         break
 
-
-  start_game():
+    start_game()
