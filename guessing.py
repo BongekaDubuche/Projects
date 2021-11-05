47295cf2f1705a794619e2c0abac4058365cf996
BongekaDubuche <bongeka.dubuche@gmail.com>
2021-11-05 11:50:13 -0600

    1st commit

--- guessing.py
+++ guessing.py
@@ -3,39 +3,43 @@ import random
 
 def start_game():
 
-    number = random.randint(1, 10)
+ def start_game():
+    number = 0
+    guess = 0
+    tries = 0
+
+
+number = random.randint(1, 10)
 
 
 name = input("Hi, What's your name? ")
 print("Ok!" + name + "Welcome to Guessing Game.!")
 
-number = input("Okay, I have picked a random number between 1 and 10. What's your guess? ")
+guess = input("Okay, I have picked a random number between 1-10. What's your guess? ")
+number = random.randint(1,10)
 try:
-    int(number)
+    int(guess)
 except ValueError:
     guess = input("That is not a valid number. Please input a number between 1-10. ")
-    tries = 1
-    guessing = 1
-    number = 0
-
-while number != guess:
-    if guess < number:
-        print("it's lower")
-        guess = input("Guess again!")
-    if guess > number:
-        print("it's higher")
-        guess = input("Guess again! ")
-        tries = tries + 1
-
-        try:
-            int(guess)
-        except ValueError:
-            guess = input("{guess} isn't a valid entry. Please input a number between 1-10.")
-    elif guess == number:
+guess = 1
+while guess != number:
+    if guess <= 0 or guess >= 10:
+        print("Please only guess a number between 1-10")
+        guess = int(input("Guess the number between 1-10"))
+    elif guess < number:
+        print("It's too lower")
+        guess = int(input("Guess the number between 1-10"))
+    elif guess > number:
+        print("It's too Higher")
+        guess = int(input("Guess the number between 1-10"))
+    else:
+        break
+    if guess == number:
         print("Good job!," + name + "! You got it right!")
-        print("Your guessed the number in: ," + str(guess) + "tries!")
+        print("Your guessed the number in ," + str(guess) + "tries!")
         print("Game Over!!")
 
         break
 
-    start_game()
+start_game()
+
