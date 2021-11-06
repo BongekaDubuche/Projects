f020ff8cd915f436fc56d3798c8e71ef308d6aa3
BongekaDubuche <bongeka.dubuche@gmail.com>
2021-11-05 20:10:52 -0600

    1st commit

--- guessing.py
+++ guessing.py
@@ -1,43 +1,44 @@
 import random
 
 
-def start_game():
-
- def start_game():
-    number = 0
-    guess = 0
-    tries = 0
-
-
-number = random.randint(1, 10)
-
-
 name = input("Hi, What's your name? ")
 print("Ok!" + name + "Welcome to Guessing Game.!")
-
 guess = input("Okay, I have picked a random number between 1-10. What's your guess? ")
-number = random.randint(1,10)
 try:
     int(guess)
 except ValueError:
     guess = input("That is not a valid number. Please input a number between 1-10. ")
-guess = 1
+
+    number = 0
+guess_number = 0
+count_number_of_tries = 0
+
+
+def start_game():
+
+ number = random.randint(1, 10)
+
+
+count_number_of_tries = 1
+number = int(input('Please guess a number between 1 and 10: '))
 while guess != number:
-    if guess <= 0 or guess >= 10:
-        print("Please only guess a number between 1-10")
-        guess = int(input("Guess the number between 1-10"))
-    elif guess < number:
+    print("Try again! ")
+    if count_number_of_tries == 4:
+        break
+    elif guess_number < number:
         print("It's too lower")
-        guess = int(input("Guess the number between 1-10"))
-    elif guess > number:
+    else:
         print("It's too Higher")
         guess = int(input("Guess the number between 1-10"))
-    else:
-        break
-    if guess == number:
+        count_number_of_tries += 1
+
+    if number == guess:
         print("Good job!," + name + "! You got it right!")
-        print("Your guessed the number in ," + str(guess) + "tries!")
-        print("Game Over!!")
+        print("You took" + str(count_number_of_tries) + "tries!")
+    else:
+        print("Sorry! You did not make it.")
+        print("The number you needed to guess was", number)
+        print("Game Over")
 
         break
 
